7e36b7858485e941261a93c6103ed991ade78422
sorasin91 <sora.sin91@gmail.com>
2023-11-25 23:48:15 +0800

    added day 7 assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [data,setData] = useState([])
+
+  const [newName,setnewName] = useState()
+  const [newEmail,setnewEmail] = useState()
+
+  const AddData = () => {
+    let newDatas = { name: newName.trim(), email: newEmail.trim() }
+    setData([...data,newDatas])
+    setnewName('')
+    setnewEmail('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <form>
+          <table>
+            <tr>
+              <td><label>Name:</label></td>
+              <td><input type='text' placeholder='Enter your name' value={newName} onChange={(e) => setnewName(e.target.value)} /></td>
+            </tr>
+            <tr>
+              <td><label>Email:</label></td>
+              <td><input type='email' placeholder='Enter your email' value={newEmail} onChange={(e) => setnewEmail(e.target.value)} /></td>
+            </tr>
+          </table>
+          <br/>
+          <button type='button' onClick={AddData}>Submit</button>
+        </form>
+      </center>
+
+      
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+          {data.map((item) => (
+            <tr>
+              <th>{item.name}</th>
+              <th>{item.email}</th>
+            </tr>
+          ))
+          }
+        </table>
+      
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
